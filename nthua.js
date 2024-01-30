e7bb4a69f70cb533e344ed91645b6c16dd0ece29
pvnotpv <66077878+pvnotpv@users.noreply.github.com>
2024-01-30 20:09:48 +0530

    Create nthua.js

---
+++ nthua.js
@@ -0,0 +1,12 @@
+// @name block
+// @description blocks webpages with certain keywords
+// @author pvnotpv
+// @version 1.0
+// @match *
+// @match https://*/*
+// @match http://*/*
+// @include     *
+
+<script>
+  document.body.style.backgroundColor = "black";
+</script>
