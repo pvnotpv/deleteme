625a6d63abf866b1e6adfabeb52a652b59a79766
pvnotpv <66077878+pvnotpv@users.noreply.github.com>
2024-01-30 11:10:09 +0530

    Create asdf.js

---
+++ asdf.js
@@ -0,0 +1,10 @@
+// @name block
+// @description blocks webpages with certain keywords
+// @author pvnotpv
+// @version 1.0
+// @match *
+
+window.onload = function() {
+    // similar behavior as clicking on a link
+    window.location.href = "http://stackoverflow.com";
+}
