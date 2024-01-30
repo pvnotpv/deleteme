ecff8e9094417e3771e879947d92cd043d421206
pvnotpv <66077878+pvnotpv@users.noreply.github.com>
2024-01-30 19:47:19 +0530

    Update meow.js

--- meow.js
+++ meow.js
@@ -1,4 +1,11 @@
-
+// @name block
+// @description blocks webpages with certain keywords
+// @author pvnotpv
+// @version 1.0
+// @match *
+// @match https://*/*
+// @match http://*/*
+// @include     *
 
 window.addEventListener('load', function (){
     alert('Function #1');
